236d2d062794d3f21b4fa007557cbd4a5a938f37
☃ pitr <pitr.vern@gmail.com>
2013-05-03 22:26:27 -0400

    prepare for deploy

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/deployer/apps/statusus/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.statusus.sock"
+worker_processes 2
+timeout 30
