6a5c32a07ad7c250e679d28bc0c71d363b95566c
☃ pitr <pitr.vern@gmail.com>
2013-06-27 20:02:57 -0400

    analyze the whole host URL for CNAME match, minus www. part

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     @user = if params[:uuid]
       User.where(uuid: params[:uuid]).first
     else
-      User.where(cname: request.host).first
+      User.where(cname: request.host.gsub(/^www./, '')).first
     end
     @feeds = @user.feeds.includes(:messages) # TODO: remove includes
     @hide_navbar = true
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,20 @@
 Statusus::Application.routes.draw do
-  resources :feeds, :only => [:index, :create] do
-    resources :messages, :only => :create
+  resources :feeds, only: [:index, :create] do
+    resources :messages, only: :create
   end
 
-  devise_for :users, :controllers => { :registrations => :registrations }
+  devise_for :users, controllers: { registrations: :registrations }
 
   get '/pricing' => 'application#pricing'
 
   get '/dashboard/:uuid' => 'dashboard#show', :as => :dashboard
-  get '/' => 'dashboard#show', :constraints => lambda { |r| r.subdomain.present? && r.subdomain != 'www' }
 
-  post "heroku/resources" => "heroku#create"
-  put "heroku/resources/:id" => "heroku#update"
-  delete "heroku/resources/:id" => "heroku#destroy"
-  post "heroku/login" => "heroku#login"
+  root to: 'dashboard#show', constraints: ->(request) { User.exists?(cname: request.host.gsub(/^www./, '')) }
 
-  root :to => 'application#welcome'
+  post    'heroku/resources'      => 'heroku#create'
+  put     'heroku/resources/:id'  => 'heroku#update'
+  delete  'heroku/resources/:id'  => 'heroku#destroy'
+  post    'heroku/login'          => 'heroku#login'
+
+  root to: 'application#welcome'
 end
